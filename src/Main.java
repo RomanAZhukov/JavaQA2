29ac3b0a87eb6460d70b78c3239af3ae166e9ecf
Роман Жуков <zhukovroman82@gmail.com>
2022-05-24 18:05:33 +0300

    New

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 100;
+        int perMile = 20;
+        int bonus = price / perMile;
+        System.out.println("Бесплатные мили:" + bonus);
+    }
+
+
+}
